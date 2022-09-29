5a85a05840018de0fd923fadac33c6949436086a
PIERROLL <ppiero119@gmail.com>
2022-09-28 20:47:24 -0500

    Tercera Revisión

---
+++ src/SEMANA_02/Ejercicio_04.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//Carclular la hipotenusa
+package SEMANA_02;
+import java.util.Scanner;
+/**
+ *
+ * @author ppier
+ */
+public class Ejercicio_04 {
+   public static void main(String[] args) {
+        
+    double cateto_01, cateto_02;
+    Scanner sc=new Scanner(System.in);
+       System.out.println("Introducción el valor del primer cateto: "); 
+       cateto_01=sc.nextDouble();  
+       System.out.println("Introducción el valor del segundo cateto: "); 
+       cateto_02=sc.nextDouble();  
+       double Hipotenusa=Math.sqrt(Math.pow(cateto_01,2)+Math.pow(cateto_02, 2));
+       System.out.println("La hipotenusa es:"+Hipotenusa);
+
+     }
+}
